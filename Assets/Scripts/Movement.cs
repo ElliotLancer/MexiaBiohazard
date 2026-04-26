ce4b9eccffdef3e679ce7ebe6a503bcc3a3a6061
ElliotLancer <nursagindik4@gmail.com>
2026-04-26 20:09:23 +0500

    Added crouch system and animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,24 +3,46 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _crouchSpeed;
+    [SerializeField] private BoxCollider2D _collider;
+    [SerializeField] private Vector2 _crouchSize = new Vector2(1.13f, 3.7f);
+    [SerializeField] private Vector2 _crouchOffset = new Vector2(0f, -0.25f);
+
+    private Vector2 _standSize;
+    private Vector2 _standOffset;
     private Rigidbody2D _rb;
     private float _moveInput;
     public Animator animator;
+    private bool _isCrouching = false;
     
-
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _standSize = _collider.size;
+        _standOffset = _collider.offset;
     }
     private void Update()
     {
         _moveInput = Input.GetAxis("Horizontal");
         float speed = Mathf.Abs(_moveInput);
         animator.SetFloat("Speed", speed);
+        _isCrouching = Input.GetKey(KeyCode.LeftControl);
+        if (_isCrouching)
+        {
+            _collider.size = _crouchSize;
+            _collider.offset = _crouchOffset;
+        }
+        else
+        {
+            _collider.size = _standSize;
+            _collider.offset = _standOffset;
+        }
+        animator.SetBool("IsCrouching", _isCrouching);
     }
     private void FixedUpdate()
     {
-        _rb.linearVelocity = new Vector2(_moveInput * _movementSpeed, _rb.linearVelocity.y);
+        float speed = _isCrouching ? _crouchSpeed : _movementSpeed;
+        _rb.linearVelocity = new Vector2(_moveInput * speed, _rb.linearVelocity.y);
     }
 
 }
