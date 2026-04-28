da6678118e9602ae738c39b10772d646cd0cef33
ElliotLancer <nursagindik4@gmail.com>
2026-04-28 13:16:32 +0500

    ref: changed ignoreCollision script

--- Assets/Scripts/EnemyRagdoll.cs
+++ Assets/Scripts/EnemyRagdoll.cs
@@ -46,5 +46,5 @@ public class EnemyRagdoll : MonoBehaviour
             rb.simulated = false;
         }
     }
-
+   
 }
--- Assets/Scripts/ignoreCollision.cs
+++ Assets/Scripts/ignoreCollision.cs
@@ -1,23 +1,16 @@
 using UnityEngine;
 
-public class ignoreCollision : MonoBehaviour
+public class IgnoreCollision : MonoBehaviour
 {
-    [SerializeField] private Collider2D _body;
-    [SerializeField] private Collider2D _arm;
-    [SerializeField] private Collider2D _arm1;
-    [SerializeField] private Collider2D _leg;
-    [SerializeField] private Collider2D _leg1;
-    [SerializeField] private Collider2D _head;
-
-    private void Start()
+    private void Awake()
     {
-        Physics2D.IgnoreCollision(_body, _arm);
-        Physics2D.IgnoreCollision(_body, _arm1);
-        Physics2D.IgnoreCollision(_body, _leg1);
-        Physics2D.IgnoreCollision(_head, _body);
-        Physics2D.IgnoreCollision(_head, _arm);
-        Physics2D.IgnoreCollision(_head, _arm1);
-        Physics2D.IgnoreCollision(_arm1, _leg);
-        Physics2D.IgnoreCollision(_arm1, _leg1);
+        Collider2D[] colliders = GetComponentsInChildren<Collider2D>();
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            for (int j = i + 1; j < colliders.Length; j++)
+            {
+                Physics2D.IgnoreCollision(colliders[i], colliders[j], true);
+            }
+        }
     }
 }
