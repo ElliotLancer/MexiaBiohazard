a91b4ec0dab57605827bc5adc6c0cc253562e387
ElliotLancer <nursagindik4@gmail.com>
2026-05-01 15:11:38 +0500

    added zombieMan attack animation

--- Assets/Scripts/Kill.cs
+++ Assets/Scripts/Kill.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 public class Kill : MonoBehaviour
 {
     public PlayerHealth health;
-    public ZombieEnemyHealth health2;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
             health.Die();
-            health2.Die();
         }
     }
 }
