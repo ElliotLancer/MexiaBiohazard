9ae1499f8c0cf9efbe1f7de67e2c43a444a84c70
ElliotLancer <nursagindik4@gmail.com>
2026-06-16 13:04:24 +0500

    Added ammo drops based on weapons in inventory

--- Assets/Scripts/SpawnItems.cs
+++ Assets/Scripts/SpawnItems.cs
@@ -4,10 +4,16 @@ public class SpawnItems : MonoBehaviour
     [SerializeField] private GameObject _coin;
     [SerializeField] private GameObject _dollar;
     [SerializeField] private GameObject _pistolAmmo;
+    [SerializeField] private GameObject _rifleAmmo;
+    [SerializeField] private GameObject _shotgunAmmo;
 
-    [SerializeField] private float _pistolAmmoChance = 0.3f;
-    [SerializeField] private int _pistolAmmoMin = 1;
-    [SerializeField] private int _pistolAmmoMax = 2;
+    [SerializeField] private float _secondaryAmmoChance = 0.3f;
+    [SerializeField] private int _secondaryAmmoMin = 1;
+    [SerializeField] private int _secondaryAmmoMax = 3;
+
+    [SerializeField] private float _primaryAmmoChance = 0.2f;
+    [SerializeField] private int _primaryAmmoMin = 1;
+    [SerializeField] private int _primaryAmmoMax = 2;
 
     [SerializeField] private float _coinChance = 0.5f;
     [SerializeField] private float _dollarChance = 0.2f;
@@ -39,14 +45,37 @@ public class SpawnItems : MonoBehaviour
                 rb.AddForce(Random.insideUnitCircle * 1f, ForceMode2D.Impulse);
             }
         }
-        if (Random.value < _pistolAmmoChance)
+        if (Random.value < _secondaryAmmoChance)
         {
-            int pistolAmmoAmount = Random.Range(_pistolAmmoMin, _pistolAmmoMax);
+            int pistolAmmoAmount = Random.Range(_secondaryAmmoMin, _secondaryAmmoMax);
             for (int i = 0; i < pistolAmmoAmount; i++)
             {
                 GameObject pistolAmmo = Instantiate(_pistolAmmo, pos.position, Quaternion.identity);
                 Rigidbody2D rb = pistolAmmo.GetComponent<Rigidbody2D>();
                 rb.AddForce(Random.insideUnitCircle * 1f, ForceMode2D.Impulse);
+            }
+        }
+        if (Random.value < _primaryAmmoChance)
+        {
+            string primaryWeapon = PlayerPrefs.GetString("PrimaryWeapon", "");
+            int primaryAmmoAmount = Random.Range(_primaryAmmoMin, _primaryAmmoMax);
+            if (primaryWeapon == "rifle")
+            {
+                for (int i = 0; i < primaryAmmoAmount; i++)
+                {
+                    GameObject rifleAmmo = Instantiate(_rifleAmmo, pos.position, Quaternion.identity);
+                    Rigidbody2D rb = rifleAmmo.GetComponent<Rigidbody2D>();
+                    rb.AddForce(Random.insideUnitCircle * 1f, ForceMode2D.Impulse);
+                }
+            }
+            if (primaryWeapon == "shotgun")
+            {
+                for (int i = 0; i < primaryAmmoAmount; i++)
+                {
+                    GameObject shotgunAmmo = Instantiate(_shotgunAmmo, pos.position, Quaternion.identity);
+                    Rigidbody2D rb = shotgunAmmo.GetComponent<Rigidbody2D>();
+                    rb.AddForce(Random.insideUnitCircle * 1f, ForceMode2D.Impulse);
+                }
             }
         }
     }
